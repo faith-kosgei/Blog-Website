e2aabfdf88987fe380eb684a54925e6df5a77408
faith-kosgei <kosgeif7@gmail.com>
2023-06-05 07:17:31 +0300

    more changes

--- api/routes/user.js
+++ api/routes/user.js
@@ -34,22 +34,25 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
     if(req.params.userId === req.params.id){
         // delete all post of  user and user account
-        
-
         try{
             const user = await User.findById(req.params.id)
 
             try {
                 await Post.deleteMany({username:user.username})
+
+                // only delete user account
+
+                await User.findByIdAndDelete(req.params.id)
+                res.status(200).json("user has been deleted....")
             } catch (error) {
-
-                
+                res.status(500).json(error) 
             }
-
-
-        }catch(error){
+        } catch(error){
+            res.status(404).json("user not found....")
 
         }
+    }else{
+        res.status(401).json("You can only delete your account....")
     }
 })
 
